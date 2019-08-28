63da6fa76ec4cd56e51577887a40ebba2679187f
Peter Carmichael <peter.carmichael22@gmail.com>
2019-08-28 13:38:42 -0700

    lab 12 wednesday submission

--- data/books.sql
+++ data/books.sql
@@ -7,8 +7,21 @@ CREATE TABLE books (
  description VARCHAR,
  isbn NUMERIC(13),
  thumbnail VARCHAR(255),
- pagecount NUMERIC(5),
- infolink VARCHAR(255),
- rating VARCHAR(255),
  genre VARCHAR(255)
+);
+
+INSERT INTO books (
+  author,
+  title,
+  isbn,
+  thumbnail,
+  description,
+  genre
+) VALUES (
+  'Frank Herbert',
+  'Dune',
+  '9780441013593',
+  'http://books.google.com/books/content?id=B1hSG45JCX4C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  'Follows the adventures of Paul Atreides, the son of a betrayed duke given up for dead on a treacherous desert planet and adopted by its fierce, nomadic people, who help him unravel his most unexpected destiny.',
+  'our first books'
 );
--- server.js
+++ server.js
@@ -20,13 +20,23 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-app.get('/', (req, res) => {
-    return res.render('./pages/index');
-});
-
+app.get('/', getBooks);
+app.get('/book-search', newSearch);
 app.post('/book-search', searchForBook);
 
 // route handlers
+
+function newSearch(request, response) {
+    return response.render('./pages/searches/new');
+}
+
+function getBooks(req, res) {
+    const SQL = `SELECT * FROM books;`
+    return client.query(SQL)
+        .then((results) => {
+            res.render('pages/index', { results });
+        })
+}
 
 function searchForBook(req, res) {
     const searchType = req.body.search[0];
@@ -48,24 +58,13 @@ function searchForBook(req, res) {
         const formattedBookList = bookList.map(bookData => {
             const regex = /(http)/g;
             return new Book(bookData.volumeInfo.title,
-                bookData.volumeInfo.authors[0],
+                bookData.volumeInfo.authors && bookData.volumeInfo.authors[0],
                 bookData.volumeInfo.description,
-                bookData.volumeInfo.industryIdentifiers[0].identifier,
                 bookData.volumeInfo.imageLinks.smallThumbnail.replace(regex, 'https'),
-                bookData.volumeInfo.pageCount,
-                bookData.volumeInfo.infoLink,
-                bookData.volumeInfo.averageRating);
+            );
         })
+        console.log(formattedBookList[0].isbn)
         res.render('./pages/searches/show', { myData: formattedBookList });
-        //Logging data into the SQL DB
-        formattedBookList.forEach(eachBook => {
-            const sqlQueryInsert = `
-   INSERT INTO books (title, author, description, isbn, thumbnail, pagecount, infolink, rating)
-   VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
-            const valuesArray = [eachBook.title, eachBook.author, eachBook.description, eachBook.isbn, eachBook.thumbnail, eachBook.pagecount, eachBook.link, eachBook.rating];
-            //client.query takes in a string and array
-            client.query(sqlQueryInsert, valuesArray);
-        })
     }).catch(error => {
         res.render('./pages/error');
         console.error(error);
@@ -76,13 +75,22 @@ app.listen(PORT, () => console.log(`Up and running on ${PORT}`));
 
 // book constructor
 
-function Book(title, author, description, isbn, thumbnail, pagecount, link, rating) {
+function Book(title, author, description, thumbnail) {
     this.title = title ? title : 'No Title Found';
     this.author = author ? author : 'No Author Found';
     this.description = description ? description : 'N/A';
-    this.isbn = isbn ? isbn : 'N/A';
     this.thumbnail = thumbnail ? thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
-    this.pagecount = pagecount ? pagecount : 'No pagecount found';
-    this.link = link ? link : 'No link found';
-    this.rating = rating ? rating : 'No rating found';
 };
+
+
+function saveToSql(req, res) {
+    //Logging data into the SQL DB
+    formattedBookList.forEach(eachBook => {
+        const sqlQueryInsert = `
+INSERT INTO books (title, author, description, thumbnail,)
+VALUES ($1, $2, $3, $4);`;
+        const valuesArray = [eachBook.title, eachBook.author, eachBook.description, eachBook.thumbnail];
+        //client.query takes in a string and array
+        client.query(sqlQueryInsert, valuesArray);
+    })
+}
