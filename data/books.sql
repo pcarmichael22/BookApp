4473a68458d2ba5d03d993d6f25e576be02d8d8d
Peter Carmichael <peter.carmichael22@gmail.com>
2019-08-28 10:30:01 -0700

    writing to db

--- data/books.sql
+++ data/books.sql
@@ -1,12 +1,14 @@
-DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS books;
 
-CREATE TABLE locations (
+CREATE TABLE books (
  id SERIAL PRIMARY KEY,
  title VARCHAR(255),
  author VARCHAR(255),
  description VARCHAR,
+ isbn NUMERIC(13),
  thumbnail VARCHAR(255),
  pagecount NUMERIC(5),
  infolink VARCHAR(255),
- rating VARCHAR(255)
+ rating VARCHAR(255),
+ genre VARCHAR(255)
 );
--- server.js
+++ server.js
@@ -2,6 +2,15 @@
 
 const express = require('express');
 const superagent = require('superagent');
+const pg = require('pg');
+require('dotenv').config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// Connect to database
+const client = new pg.Client(DATABASE_URL);
+client.connect();
+client.on('error', (error) => console.log(error));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-app.get('/test', (req, res) => {
+app.get('/', (req, res) => {
     return res.render('./pages/index');
 });
 
@@ -41,12 +50,22 @@ function searchForBook(req, res) {
             return new Book(bookData.volumeInfo.title,
                 bookData.volumeInfo.authors[0],
                 bookData.volumeInfo.description,
+                bookData.volumeInfo.industryIdentifiers[0].identifier,
                 bookData.volumeInfo.imageLinks.smallThumbnail.replace(regex, 'https'),
                 bookData.volumeInfo.pageCount,
                 bookData.volumeInfo.infoLink,
                 bookData.volumeInfo.averageRating);
         })
         res.render('./pages/searches/show', { myData: formattedBookList });
+        //Logging data into the SQL DB
+        formattedBookList.forEach(eachBook => {
+            const sqlQueryInsert = `
+   INSERT INTO books (title, author, description, isbn, thumbnail, pagecount, infolink, rating)
+   VALUES ($1, $2, $3, $4, $5, $6, $7, $8);`;
+            const valuesArray = [eachBook.title, eachBook.author, eachBook.description, eachBook.isbn, eachBook.thumbnail, eachBook.pagecount, eachBook.link, eachBook.rating];
+            //client.query takes in a string and array
+            client.query(sqlQueryInsert, valuesArray);
+        })
     }).catch(error => {
         res.render('./pages/error');
         console.error(error);
@@ -57,10 +76,11 @@ app.listen(PORT, () => console.log(`Up and running on ${PORT}`));
 
 // book constructor
 
-function Book(title, author, description, thumbnail, pagecount, link, rating) {
+function Book(title, author, description, isbn, thumbnail, pagecount, link, rating) {
     this.title = title ? title : 'No Title Found';
     this.author = author ? author : 'No Author Found';
     this.description = description ? description : 'N/A';
+    this.isbn = isbn ? isbn : 'N/A';
     this.thumbnail = thumbnail ? thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
     this.pagecount = pagecount ? pagecount : 'No pagecount found';
     this.link = link ? link : 'No link found';
